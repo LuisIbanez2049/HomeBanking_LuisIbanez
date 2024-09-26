898f3907c518ccc723d0330b90e6df197e04d1ca
unknown <luiszyzz248@gmail.com>
2024-09-26 16:09:15 -0300

    web config

--- src/main/java/com/mindhub/homebanking/controllers/TestController.java
+++ src/main/java/com/mindhub/homebanking/controllers/TestController.java
@@ -1,12 +1,15 @@
 package com.mindhub.homebanking.controllers;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/api")
 public class TestController {
+
 
     @GetMapping("/message")
     private String messageController() {
