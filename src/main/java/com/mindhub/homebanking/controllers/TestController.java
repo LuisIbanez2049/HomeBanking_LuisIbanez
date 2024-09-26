1bb8a658bed4296bd65aec813c231f67aa7b8ccd
unknown <luiszyzz248@gmail.com>
2024-09-26 16:04:59 -0300

    Message controller

--- src/main/java/com/mindhub/homebanking/controllers/TestController.java
+++ src/main/java/com/mindhub/homebanking/controllers/TestController.java
@@ -1,5 +1,6 @@
 package com.mindhub.homebanking.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class TestController {
 
+    @GetMapping("/message")
     private String messageController() {
         return "Respuesta de test controller";
     }
